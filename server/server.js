48103bc03a72bd15da4958d10596875fb2a7b224
baljindesiidhu <baljindesiidhu@gmail.com>
2026-05-17 19:19:08 +0530

    fixed smtp

--- server/server.js
+++ server/server.js
@@ -14,14 +14,22 @@ app.post("/send-quote", async (req, res) => {
 
     const { name, phone, email, service, date, message } = req.body;
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
+   const transporter = nodemailer.createTransport({
+  service: "gmail",
 
-      auth: {
-  user: process.env.EMAIL_USER,
-  pass: process.env.EMAIL_PASS
-}
-    });
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+await transporter.verify();
+
+console.log("SMTP Ready");
 
     await transporter.sendMail({
       from: email,
