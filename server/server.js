8d3b76bab64ffdf1b08535fb52d6e85b9ea14018
baljindesiidhu <baljindesiidhu@gmail.com>
2026-05-17 18:10:51 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/send-quote", async (req, res) => {
+  try {
+
+    const { name, phone, email, service, date, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+
+      auth: {
+  user: process.env.EMAIL_USER,
+  pass: process.env.EMAIL_PASS
+}
+    });
+
+    await transporter.sendMail({
+      from: email,
+
+      to: process.env.EMAIL_USER,
+
+      subject: "New Quote Request",
+
+      html: `
+        <h2>New Quote Request</h2>
+
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Phone:</b> ${phone}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Service:</b> ${service}</p>
+        <p><b>Date:</b> ${date}</p>
+        <p><b>Message:</b> ${message}</p>
+      `
+    });
+
+    res.status(200).json({
+      success: true
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      success: false
+    });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
